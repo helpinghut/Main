b5566334cd038c040772d8377109a66925c82c67
helpinghut <98209261+helpinghut@users.noreply.github.com>
2022-02-13 18:42:38 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,62 @@
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+  function myFunction1() {
+    var dots = document.getElementById("dot");
+    var moreText = document.getElementById("More");
+    var btnText = document.getElementById("myBtn1");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+
+  function myFunction2() {
+    var dots = document.getElementById("dot1");
+    var moreText = document.getElementById("More1");
+    var btnText = document.getElementById("myBtn2");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+
+
+function changeClass() {
+  var content = document.getElementById("extra_content");
+  var btn = document.getElementById("read_more");
+  content.classList.toggle('show');
+
+  if (content.classList.contains("show")) {
+      btn.innerHTML = "Show Less";
+  } else {
+      btn.innerHTML = "Show More";
+  }
+}
