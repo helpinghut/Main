dfe5eb94a8cbbd0653c99d68a32e659d4f3fe3f7
Mohammed Moazzam Khan <kmohdmoazzam70@gmail.com>
2022-02-13 16:49:37 +0530

    Added Team,Celebration,Drive,Campaign page

---
+++ CELEBRATIONS.js
@@ -0,0 +1,82 @@
+
+
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
+
+$("#slideshow1 > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow1 > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow1');
+},  3000);
+
+$("#slideshow2 > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow2 > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow2');
+},  3000);
+
+$("#slideshow3 > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow3 > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow3');
+},  3000);
+
+$("#slideshow4 > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow4 > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow4');
+},  3000);
+
+$("#slideshow5 > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow5 > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow5');
+},  3000);
+
+$("#slideshow6 > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow6 > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow6');
+},  3000);
+
+
+
