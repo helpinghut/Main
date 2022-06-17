66755a3f1064e2eee9c55e7cdbedaf0826285528
Shreyas Choudhari <96518300+shreyaschoudhari41@users.noreply.github.com>
2022-06-17 16:14:23 +0530

    Update footer-new.js
    Signed-off-by: Shreyas Choudhari  <96518300+shreyaschoudhari41@users.noreply.github.com>

--- footer-new.js
+++ footer-new.js
@@ -55,3 +55,23 @@ fetch('./footer-new.html').then(function (response) {
     let footer = document.getElementsByTagName('footer')[0];
     footer.innerHTML = html;
 });
+
+//Get the button:
+mybutton = document.getElementById("myBtnn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () { scrollFunction() };
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         mybutton.style.display = "block";
+//     } else {
+//         mybutton.style.display = "none";
+//     }
+// }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
